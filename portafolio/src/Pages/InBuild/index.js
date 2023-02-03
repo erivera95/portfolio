7873eb3bfcc4a3b2d757b382a79cd3b8b72ef47c
SurfaceEduardo <erivera95@outlook.com>
2023-02-03 14:21:26 -0600

    Creacion container pagina de ENCONSTRUCCION

---
+++ portafolio/src/Pages/InBuild/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+
+const inBuild = () => {
+    return (
+        <div>
+            <div class="loader">
+                <span class="loader__element"></span>
+                <span class="loader__element"></span>
+                <span class="loader__element"></span>
+            </div>
+        </div>
+    )
+}
+
+export default inBuild
---
+++ portafolio/src/Pages/PomodoroApp/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pomodoro = () => {
+  return (
+    <div>pomodoro</div>
+  )
+}
+
+export default pomodoro
